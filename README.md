530cc50b200fe83933e1c743db02f45495f8249c
Divya <divyanamburu68@gmail.com>
2026-07-04 10:47:29 +0530

    Completed Task 4 - Object Detection and Tracking

---
+++ README.md
@@ -0,0 +1,21 @@
+# Object Detection and Tracking
+
+## Description
+
+This project performs real-time object detection using OpenCV. The system captures live video from a webcam and detects human faces by drawing bounding boxes around them.
+
+## Technologies Used
+
+- Python
+- OpenCV
+
+## Features
+
+- Real-time webcam access
+- Face detection using AI techniques
+- Bounding box visualization
+- Live detection output
+
+## Author
+
+mohammad sami
---
+++ object_detection_ai.py
@@ -0,0 +1,55 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades +
+    'haarcascade_frontalface_default.xml'
+)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(
+        frame,
+        cv2.COLOR_BGR2GRAY
+    )
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        1.1,
+        4
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(
+            frame,
+            (x, y),
+            (x+w, y+h),
+            (0, 255, 0),
+            2
+        )
+
+        cv2.putText(
+            frame,
+            "Face Detected",
+            (x, y-10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (0,255,0),
+            2
+        )
+
+    cv2.imshow(
+        "AI Object Detection",
+        frame
+    )
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
